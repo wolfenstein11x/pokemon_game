de4880f0671668be57752a1cfc277d4710e33c48
Charles Wolfe <c.wolfe132@gmail.com>
2020-08-09 22:14:08 -0700

    added comments

--- pokemon_game.py
+++ pokemon_game.py
@@ -3,28 +3,34 @@ import time
 
 pygame.init()
 
+# dimensions of game window
 display_width = 800
 display_height = 600
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Pokemon Game')
 
+# colors
 black = (0,0,0)
 white = (255,255,255)
 
+# image sizes
 pkm_width = 136
 pkm_height = 151
 
 clock = pygame.time.Clock()
 pkm_img = pygame.image.load('blastoise.png')
 
+# function to display pokemon
 def pkm(x,y):
     gameDisplay.blit(pkm_img, (x,y))
 
+# helper function for message_display
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
+# function to display text to screen
 def message_display(text):
     largeText = pygame.font.Font('freesansbold.ttf', 115)
     TextSurf, TextRect = text_objects(text, largeText)
@@ -35,7 +41,10 @@ def message_display(text):
     pygame.display.update()
     time.sleep(2)
 
+# game events set in the game_loop function
 def game_loop():
+    
+    # starting position
     x = (display_width * 0.45)
     y = (display_height * 0.7)
     x_change = 0
@@ -48,6 +57,7 @@ def game_loop():
             if event.type == pygame.QUIT:
                 gameExit = True
 
+            # arrow keys for motion
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
@@ -67,6 +77,7 @@ def game_loop():
         x += x_change
         y += y_change
 
+        # prevent pokemon from running off the page
         if x <= 0:
             x = 0
         if x >= (display_width - pkm_width):
@@ -77,12 +88,14 @@ def game_loop():
         if y >= (display_height - pkm_height):
             y = (display_height - pkm_height)
 
+        # first paint background, then paint pokemon over background
         gameDisplay.fill(white)
         pkm(int(x),int(y))
 
         pygame.display.update()
         clock.tick(60)
 
+# main function
 message_display("Round 1")
 message_display("Fight!")
 game_loop()
